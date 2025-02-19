a9075bf7b367739713af681d27bfca104764a33a
rrhmn <raissarahmani18@gmail.com>
2025-02-19 18:19:17 +0700

    week 2 day 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import weatherCheck from "./src/getData.js";
 const dataCheck = (city) => {
     if(typeof city !== "string") return
     const cityData = weatherCheck(city)
-    // if(!(weatherCheck instanceof Array)) return
+    if(!(cityData instanceof Array)) return
     console.log(`Data cuaca di ${city}: ${cityData}`)
 }
 dataCheck("makassar")
--- src/getData.js
+++ src/getData.js
@@ -1,9 +1,8 @@
 const API_KEY = "70e248149c2748ab8f2e276f831a1e40"
-const API = "https://api.openweathermap.org/data/2.5/weather?q="
+const API = "https://api.openweathermap.org/data/2.5/weather?"
 
 const weatherCheck = (city) => {
-    const url = `${API}${city}&appid=${API_KEY}`
-    console.log(url);
+    const url = `${API}q=${city}&appid=${API_KEY}`
     fetch (url)
     .then((response) => {
         if (!response.ok) throw new Error(response.statusText)

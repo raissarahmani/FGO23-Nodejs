a4ca2d11d54a51c0d497c54d801a08d674d77988
rrhmn <raissarahmani18@gmail.com>
2025-02-19 17:09:24 +0700

    week 2 day 2

---
+++ index.js
@@ -0,0 +1,18 @@
+const API_KEY = "70e248149c2748ab8f2e276f831a1e40"
+const API = "https://api.openweathermap.org/data/2.5/weather?q="
+
+const weatherCheck = (city) => {
+    const url = `${API}${city}&appid=${API_KEY}`
+    fetch (url)
+    .then((response) => {
+        if (!response.ok) throw new Error(response.statusText)
+        return response.json()
+    })
+    .then((weatherCity) => {
+        console.log(weatherCity.weather)
+    })
+    .catch((err) => {
+        if(err instanceof Error) console.log(err.message)
+    })
+}
+weatherCheck("surabaya")
